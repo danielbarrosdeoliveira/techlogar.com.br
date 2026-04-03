66165097b5bda1cd29300fba290ab99eb8dcea30
Daniel Oliveira <d.dani_oli@hotmail.com>
2026-04-03 11:30:36 -0300

    ✨ add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,22 @@ export default defineNuxtConfig({
 	future: {
 		compatibilityVersion: 4,
 	},
+	app: {
+		head: {
+			title: 'TechLogar',
+			meta: [
+				{ name: 'description', content: 'Tecnologia e Teologia de forma prática.' },
+				{ name: 'theme-color', content: '#ffffff' },
+			],
+			link: [
+				{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+				{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+				{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+				{ rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+				{ rel: 'manifest', href: '/site.webmanifest' },
+			],
+		},
+	},
 
 	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', '@nuxt/content'],
 
